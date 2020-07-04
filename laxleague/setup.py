46b5ae279aff8dc71ef17110ccef95dbf15fe962
mas192 <samjoshva@rediffmail.com>
2020-07-04 00:57:07 -0700

    setup file created

--- laxleague/setup.py
+++ laxleague/setup.py
@@ -1,8 +1,7 @@
 from setuptools import find_packages, setup
-
 setup(
     name='laxleague',
     extras_require=dict(tests=['pytest']),
     packages=find_packages(where="src"),
-    package_dir=("":"src")
+    package_dir={"": "src"},
 )
